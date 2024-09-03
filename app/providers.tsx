414fdea011750c97a78783b189fbc5431fbeeaa4
emmanuel4365 <128937350+emmanuel4365@users.noreply.github.com>
2024-09-03 16:11:35 +0100

    created providers and theme-provider components

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,8 +1,17 @@
 "use client";
 
+import { ThemeProvider } from "./theme-provider";
+
 function Providers({ children }: { children: React.ReactNode; }) {
     return (
-        <>{children}</>
+        <>
+            <ThemeProvider attribute='class'
+                defaultTheme='system'
+                enableSystem
+                disableTransitionOnChange>
+                {children}
+            </ThemeProvider>
+        </>
     );
 }
 export default Providers;

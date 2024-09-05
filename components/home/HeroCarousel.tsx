9fac82ea983a29ae5bd7b4316c404951cd7250b1
emmanuel4365 <128937350+emmanuel4365@users.noreply.github.com>
2024-09-05 17:53:31 +0100

    setup Hero carousel component

--- components/home/HeroCarousel.tsx
+++ components/home/HeroCarousel.tsx
@@ -1,6 +1,44 @@
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from '@/components/ui/carousel';
+import { Card, CardContent } from '@/components/ui/card';
+import Image from 'next/image';
+import hero1 from '@/public/images/shoes-hero1.jpg';
+import hero2 from '@/public/images/clothes-hero2.jpg';
+import hero3 from '@/public/images/phone-hero3.jpg';
+import hero4 from '@/public/images/furniture-hero4.jpg';
+
+const carouselImages = [hero1, hero2, hero3, hero4];
+
 function HeroCarousel() {
     return (
-        <div>HeroCarousel</div>
+        <div className='hidden lg:block'>
+            <Carousel>
+                <CarouselContent>
+                    {carouselImages.map((image, index) => {
+                        return (
+                            <CarouselItem key={index}>
+                                <Card>
+                                    <CardContent className='p-2'>
+                                        <Image
+                                            src={image}
+                                            alt='hero'
+                                            className='w-full h-[24rem] rounded-md object-cover'
+                                        />
+                                    </CardContent>
+                                </Card>
+                            </CarouselItem>
+                        );
+                    })}
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+            </Carousel>
+        </div>
     );
 }
 export default HeroCarousel;

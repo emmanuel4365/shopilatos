03d6dc8930d5e7217c9aac1fce248beca64c30b8
emmanuel4365 <128937350+emmanuel4365@users.noreply.github.com>
2024-09-13 01:02:30 +0100

    completed review functionality

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "otlukgsjaucujykwooxi.supabase.co",
       },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
     ],
   },
 };
